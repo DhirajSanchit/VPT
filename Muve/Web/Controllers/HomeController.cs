4d2308672687677aac1def19f2d4ff825d0681ac
Amit <Dhirajsanchit@gmail.com>
2020-06-19 12:24:29 +0200

    pt2 ASP boogaloo

---
+++ Muve/Web/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+           /**
+           if(!LoggedIn()){
+           // Send to login page
+           return Redirect("/"){
+           }
+           }
+           **/
+            return View();
+        }
+
+
+    }
+}
